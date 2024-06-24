cbc221f4aefdd9a7723a725c06e959b3dbe27b9d
Sergey Lukovkin <slukovkin@icloud.com>
2024-06-24 12:50:22 +0300

    updated authentication function

--- src/main/kotlin/com/all4drive/features/utils/comparetionHashAndPassword.kt
+++ src/main/kotlin/com/all4drive/features/utils/comparetionHashAndPassword.kt
@@ -3,6 +3,5 @@ package com.all4drive.features.utils
 import at.favre.lib.crypto.bcrypt.BCrypt
 
 fun comparePassword(passwordHash: String, password: String): Boolean {
-    val result = BCrypt.verifyer().verify(password.toCharArray(), passwordHash)
-    return result.verified
+    return BCrypt.verifyer().verify(password.toCharArray(), passwordHash).verified
 }
