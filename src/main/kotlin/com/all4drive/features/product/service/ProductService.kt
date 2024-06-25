6e0b58be20938fa2a8ffffa32de4e8df12049436
Sergey Lukovkin <slukovkin@icloud.com>
2024-06-25 19:30:38 +0300

    created crud in product module

--- src/main/kotlin/com/all4drive/features/product/service/ProductService.kt
+++ src/main/kotlin/com/all4drive/features/product/service/ProductService.kt
@@ -1,8 +1,10 @@
 package com.all4drive.features.product.service
 
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.Table
+import com.all4drive.features.product.model.Product
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class ProductService(db: Database) {
@@ -21,6 +23,85 @@ class ProductService(db: Database) {
     init {
         transaction(db) {
             SchemaUtils.create(Products)
+        }
+    }
+
+    private suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+
+    suspend fun getAllProducts(): List<Product> {
+        return dbQuery {
+            Products.selectAll()
+                .map {
+                    Product(
+                        it[Products.id],
+                        it[Products.code],
+                        it[Products.article],
+                        it[Products.title],
+                        it[Products.qty],
+                        it[Products.cross]
+                    )
+                }
+        }
+    }
+
+    suspend fun getProductByCode(code: Int): Product? {
+        return dbQuery {
+            Products.select { Products.code eq code }
+                .map {
+                    Product(
+                        it[Products.id],
+                        it[Products.code],
+                        it[Products.article],
+                        it[Products.title],
+                        it[Products.qty],
+                        it[Products.cross]
+                    )
+                }
+                .singleOrNull()
+        }
+    }
+
+    suspend fun getProductsByCross(cross: Int): List<Product> {
+        return dbQuery {
+            Products.select { Products.cross eq cross }
+                .map {
+                    Product(
+                        it[Products.id],
+                        it[Products.code],
+                        it[Products.article],
+                        it[Products.title],
+                        it[Products.qty],
+                        it[Products.cross]
+                    )
+                }
+        }
+    }
+
+    suspend fun create(product: Product): Int = dbQuery {
+        val isFound = getProductByCode(product.code)
+        if (isFound != null) return@dbQuery 0
+        Products.insert {
+            it[code] = product.code
+            it[article] = product.article
+            it[title] = product.title
+            it[qty] = product.qty
+            it[cross] = product.cross
+        }[Products.id]
+    }
+
+    suspend fun deleteProductByCode(code: Int) {
+        dbQuery {
+            Products.deleteWhere { Products.code eq code }
+        }
+    }
+
+    suspend fun updateProductByCode(code: Int, product: Product) {
+        dbQuery {
+            Products.update({ Products.code eq code }) {
+                it[title] = product.title
+                it[qty] = product.qty
+            }
         }
     }
 }
