a50752ef263e8b8de7dc5159611328ae79c373e5
Sergey Lukovkin <slukovkin@icloud.com>
2024-07-10 12:20:35 +0300

    updated

--- src/main/kotlin/com/all4drive/plugins/Routing.kt
+++ src/main/kotlin/com/all4drive/plugins/Routing.kt
@@ -1,11 +1,7 @@
-@file:Suppress("DEPRECATION")
-
 package com.all4drive.plugins
 
 import com.all4drive.features.shop.routes.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     configureAuthRouting()
@@ -13,9 +9,4 @@ fun Application.configureRouting() {
     configureProductRouting()
     configureStoreRouting()
     configureOrderRouting()
-    routing {
-        static("/") {
-            resources("static")
-        }
-    }
 }
